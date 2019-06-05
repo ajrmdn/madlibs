ca072e64d3acefb22dea85ab6aefcccb393d55b0
Abdellah Ramadan and Mike Lofgren <abdellah@>
2019-06-05 13:36:50 -0700

    Fix broken scripts in Jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
     var person2Input = $("input#person2").val();
     var animalInput = $("input#animal").val();
     var exclamationInput = $("input#exclamation").val();
-    var verbalInput = $("input#verb").val();
-    var nounImnput = $("input#noun").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
 
 
-    $(".person1").text("person1Input");
-    $(".person2").text("person2Input");
-    $(".animal").text("animalInput");
-    $(".exclamation").text("exclamationInput");
-    $(".verb").text("verbInput");
-    $(".noun").text("nounInput");
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
     $("#story").show();
 
